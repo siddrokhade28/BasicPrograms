171133a35cf9e2edd3778bbc580b8360aeb4b184
Siddanth Rokhade <siddrokhade@gmail.com>
2022-07-27 01:51:20 +0530

    Added :UC1:

---
+++ src/main/java/com/regex/Main.java
@@ -0,0 +1,27 @@
+package com.regex;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        User newUser = new User();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First name");
+        String firstName= scanner.next();
+        newUser.setFirstName(firstName);
+//        System.out.println("Enter Last name");
+//        String lastName= scanner.next();
+//        newUser.setLastName(lastName);
+//        System.out.println("Enter Email ID");
+//        String email= scanner.next();
+//        newUser.setEmail(email);
+//
+//        System.out.println("Enter password");
+//        String pswd= scanner.next();
+//        newUser.setPassword(pswd);
+
+        System.out.println(newUser);
+
+
+    }
+}
---
+++ src/main/java/com/regex/Validator.java
@@ -0,0 +1,26 @@
+package com.regex;
+
+import java.util.regex.Pattern;
+
+public class Validator {
+    //E.g. abc.xyz@bl.co.in
+    private static final String nameRegex = "^[A-Z][a-z]{3,}$";
+//    private static final String emailregex="^[A-za-z0-9]+([.][A-za-z0-9]+)?[@][a-z]+[.][a-z]{2,3}([a-z]{2})?$";
+//    private static final String phoneRegex = "^([+][0-9]{2})?[\\s]?[1-9][0-9]{9}$";
+//    private static final String passwordRegex= "^[A-Za-z]{8,}$";
+
+    public static boolean isValidName(String name) {
+        return Pattern.matches(nameRegex, name);
+    }
+//    public  static boolean isValidEmail(String email){
+//        return Pattern.matches(emailregex,email);
+//    }
+//    public  static boolean isValidPhoneNo(String phn){
+//        return Pattern.matches(phoneRegex,phn);
+//    }
+//    public static boolean isValidpassword(String pswd)
+//    {
+//        return Pattern.matches(passwordRegex,pswd);
+//    }
+
+}
