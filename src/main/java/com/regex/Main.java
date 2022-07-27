62831717fd53b62f3a1b90c7894c4007b6927f59
Siddanth Rokhade <siddrokhade@gmail.com>
2022-07-27 19:05:22 +0530

    Added:UC9 Cleared all Sample mail

--- src/main/java/com/regex/Main.java
+++ src/main/java/com/regex/Main.java
@@ -8,23 +8,21 @@ public class Main {
         User newUser = new User();
         //scanner object to take input from the user
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter First name");
-//        String firstName= scanner.next();
-//        newUser.setFirstName(firstName);
-//        System.out.println("Enter Last name");
-//        String lastName= scanner.next();
-//        newUser.setLastName(lastName);
-//        System.out.println("Enter Email ID");
-//        String email= scanner.next();
-//        newUser.setEmail(email);
-//        System.out.println("Enter Phone Number");
-//        String phn= scanner.next();
-//        newUser.setPhn(phn);
-////
+        System.out.println("Enter First name");
+        String firstName= scanner.next();
+        newUser.setFirstName(firstName);
+        System.out.println("Enter Last name");
+        String lastName= scanner.next();
+        newUser.setLastName(lastName);
+        System.out.println("Enter Email ID");
+        String email= scanner.next();
+        newUser.setEmail(email);
+        System.out.println("Enter Phone Number");
+        String phn= scanner.next();
+        newUser.setPhn(phn);
         System.out.println("Enter password");
         String password= scanner.next();
         newUser.setPassword(password);
-
         System.out.println(newUser);
 
 
--- src/main/java/com/regex/Validator.java
+++ src/main/java/com/regex/Validator.java
@@ -8,7 +8,7 @@ public class Validator {
     //Regex for First Name and Last Name
     private static final String nameRegex = "^[A-Z][a-z]{3,}$";
     //Regex for email
-    private static final String emailregex = "^[A-za-z0-9]+([.][A-za-z0-9]+)?[@][a-z]+[.][a-z]{2,3}([a-z]{2})?$";
+    private static final String emailregex = "^[A-za-z0-9]+([.+-][A-za-z0-9]+)?[@][a-z0-9]+[.][a-z]{2,3}([.][a-z]{2})?$";
     //Regex for Phone Number
     private static final String phoneRegex = "^([+][0-9]{2})?[\\s]?[1-9][0-9]{9}$";
     //Regex for Password
