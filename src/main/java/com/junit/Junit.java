f5d6cdbc2c5fbbe914d517e1ba6a058e37af0271
Siddanth Rokhade <siddrokhade@gmail.com>
2022-07-27 19:39:15 +0530

    Added: UC5 to UC9

--- src/main/java/com/junit/Junit.java
+++ src/main/java/com/junit/Junit.java
@@ -6,6 +6,7 @@ public class Junit {
     private static final String nameRegex = "^[A-Z][a-z]{3,}$";
     private static final String emailregex="^[A-za-z0-9]+([.][A-za-z0-9]+)?[@][a-z]+[.][a-z]{2,3}([a-z]{2})?$";
     private static final String phoneRegex = "^([+][0-9]{2})?[\\s]?[1-9][0-9]{9}$";
+    private static final String passwordRegex= "^[A-Za-z]{8,}$";
 
 
     public static boolean isValidName(String name) {
@@ -16,5 +17,9 @@ public class Junit {
     }
     public  static boolean isValidPhoneNo(String phn){
         return Pattern.matches(phoneRegex,phn);
+    }
+    public static boolean isValidpassword(String pswd)
+    {
+        return Pattern.matches(passwordRegex,pswd);
     }
 }
