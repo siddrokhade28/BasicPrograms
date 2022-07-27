3c8f844a491cd447c0878abd13613cb3daaa6496
Siddanth Rokhade <siddrokhade@gmail.com>
2022-07-27 19:37:33 +0530

    Added: UC3

--- src/main/java/com/junit/Junit.java
+++ src/main/java/com/junit/Junit.java
@@ -4,8 +4,13 @@ import java.util.regex.Pattern;
 
 public class Junit {
     private static final String nameRegex = "^[A-Z][a-z]{3,}$";
+    private static final String emailregex="^[A-za-z0-9]+([.][A-za-z0-9]+)?[@][a-z]+[.][a-z]{2,3}([a-z]{2})?$";
+
 
     public static boolean isValidName(String name) {
         return Pattern.matches(nameRegex, name);
+    }
+    public  static boolean isValidEmail(String email){
+        return Pattern.matches(emailregex,email);
     }
 }
