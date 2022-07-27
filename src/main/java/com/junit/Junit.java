35619a843dfef6b3c6390c1822f82ad723d9bd5c
Siddanth Rokhade <siddrokhade@gmail.com>
2022-07-27 19:38:24 +0530

    Added: UC4

--- src/main/java/com/junit/Junit.java
+++ src/main/java/com/junit/Junit.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class Junit {
     private static final String nameRegex = "^[A-Z][a-z]{3,}$";
     private static final String emailregex="^[A-za-z0-9]+([.][A-za-z0-9]+)?[@][a-z]+[.][a-z]{2,3}([a-z]{2})?$";
+    private static final String phoneRegex = "^([+][0-9]{2})?[\\s]?[1-9][0-9]{9}$";
 
 
     public static boolean isValidName(String name) {
@@ -12,5 +13,8 @@ public class Junit {
     }
     public  static boolean isValidEmail(String email){
         return Pattern.matches(emailregex,email);
+    }
+    public  static boolean isValidPhoneNo(String phn){
+        return Pattern.matches(phoneRegex,phn);
     }
 }
