1bdfb3f5c49727678700be5afe285675bbcfd63b
Siddanth Rokhade <siddrokhade@gmail.com>
2022-08-04 01:02:56 +0530

    Added:Enhanced Invoice

--- src/main/java/TDD/CabInvoice.java
+++ src/main/java/TDD/CabInvoice.java
@@ -1,35 +1,35 @@
 package TDD;
 
 public class CabInvoice {
-//    CabInvoice cabInvoice = new CabInvoice();
+    //    CabInvoice cabInvoice = new CabInvoice();
     public double fareCaluclation(double distance, double time) {
-        double totalFare= 10*distance+time;
-        if(totalFare<5){
-            totalFare=5;
+        double totalFare = 10 * distance + time;
+        if (totalFare < 5) {
+            totalFare = 5;
         }
         return totalFare;
     }
 
     public double aggregateCaluclation(double[][] rides) {
-        double aggregateFare= 0;
+        double aggregateFare = 0;
 
-        for(double[] ride :rides){
-            aggregateFare+=fareCaluclation(ride[0],ride[1]);
+        for (double[] ride : rides) {
+            aggregateFare += fareCaluclation(ride[0], ride[1]);
         }
         return aggregateFare;
     }
 
 
-//    public int totalRides(double[][] rides) {
-//        int count =0;
-//        for(double [] ride: rides){
-//            count++;
-//        }
-//        return count;
-//    }
-//
-//    public double AvgFare(double totalFare, int totalRides) {
-//        Double avgFare= totalFare/totalRides;
-//        return avgFare;
-//    }
+    public int totalRides(double[][] rides) {
+        int count = 0;
+        for (double[] ride : rides) {
+            count++;
+        }
+        return count;
+    }
+
+    public double AvgFare(double totalFare, int totalRides) {
+        Double avgFare = totalFare / totalRides;
+        return avgFare;
+    }
 }
--- src/test/java/TDD/CabInvoiceTest.java
+++ src/test/java/TDD/CabInvoiceTest.java
@@ -8,37 +8,29 @@ public class CabInvoiceTest {
 
     //test method To Check if the calculated fare is correct
     @Test
-    public void GivenDistanceTimeWhenInvoiceGeneratedShouldReturnTotalFare(){
-        double TotalFare= cabInvoice.fareCaluclation(0.1,3);
-        Assert.assertEquals(5,TotalFare,0);
+    public void GivenDistanceTimeWhenInvoiceGeneratedShouldReturnTotalFare() {
+        double TotalFare = cabInvoice.fareCaluclation(0.1, 3);
+        Assert.assertEquals(5, TotalFare, 0);
     }
 
     //test method to check if the multiple fares are calculated
     @Test
-    public void givenMultipleRidesWhenCalculatedShouldReturnAggregate(){
-        double rides[][]={
-                {5,5},
-                {8,9},
-                {7.6,5}
-        };
-        double Aggregate= cabInvoice.aggregateCaluclation(rides);
-        Assert.assertEquals(225,Aggregate,0);
+    public void givenMultipleRidesWhenCalculatedShouldReturnAggregate() {
+        double rides[][] = {{5, 5}, {8, 9}, {7.6, 5}};
+        double Aggregate = cabInvoice.aggregateCaluclation(rides);
+        Assert.assertEquals(225, Aggregate, 0);
     }
 
-//    @Test
-//    public void GivenArrayWhnCalculatedShouldRetrunToatalRidesFareAvgFare(){
-//        double rides[][]= {
-//                {5,5},
-//                {8,9},
-//                {7.6,5}
-//        };
-//        int TotalRides= cabInvoice.totalRides(rides);
-//        double TotalFare = cabInvoice.aggregateCaluclation(rides);
-//        double AvgFare = cabInvoice.AvgFare(TotalFare,TotalRides);
-//        Assert.assertEquals(3,TotalRides);
-//        Assert.assertEquals(225,TotalFare,0);
-//        Assert.assertEquals(75,AvgFare,0);
-//
-//    }
+    @Test
+    public void GivenArrayWhnCalculatedShouldRetrunToatalRidesFareAvgFare() {
+        double rides[][] = {{5, 5}, {8, 9}, {7.6, 5}};
+        int TotalRides = cabInvoice.totalRides(rides);
+        double TotalFare = cabInvoice.aggregateCaluclation(rides);
+        double AvgFare = cabInvoice.AvgFare(TotalFare, TotalRides);
+        Assert.assertEquals(3, TotalRides);
+        Assert.assertEquals(225, TotalFare, 0);
+        Assert.assertEquals(75, AvgFare, 0);
+
+    }
 
 }
