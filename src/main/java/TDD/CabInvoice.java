0f23e239340ab939007e5a100fd14bdaeab3369f
Siddanth Rokhade <siddrokhade@gmail.com>
2022-08-04 01:01:16 +0530

    Added:Mulatiple rides Fare Calculator

--- src/main/java/TDD/CabInvoice.java
+++ src/main/java/TDD/CabInvoice.java
@@ -1,8 +1,35 @@
 package TDD;
 
 public class CabInvoice {
-    public int fareCaluclation(int distance, int time) {
-        int totalFare= 10*distance+time;
+//    CabInvoice cabInvoice = new CabInvoice();
+    public double fareCaluclation(double distance, double time) {
+        double totalFare= 10*distance+time;
+        if(totalFare<5){
+            totalFare=5;
+        }
         return totalFare;
     }
+
+    public double aggregateCaluclation(double[][] rides) {
+        double aggregateFare= 0;
+
+        for(double[] ride :rides){
+            aggregateFare+=fareCaluclation(ride[0],ride[1]);
+        }
+        return aggregateFare;
+    }
+
+
+//    public int totalRides(double[][] rides) {
+//        int count =0;
+//        for(double [] ride: rides){
+//            count++;
+//        }
+//        return count;
+//    }
+//
+//    public double AvgFare(double totalFare, int totalRides) {
+//        Double avgFare= totalFare/totalRides;
+//        return avgFare;
+//    }
 }
